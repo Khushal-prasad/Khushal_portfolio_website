a152bd1430e57ecda9f61b1eaabf524d5b37f70b
Khushal-prasad <jeetuprasad277@gmail.com>
2025-04-11 10:26:35 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -28,7 +28,7 @@ const Hero = () => {
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I develop wonderful new-gen
-            <br className="sm:block hidden" />
+            <br/>
             websites and web applications.
           </p>
         </div>
